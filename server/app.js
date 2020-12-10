697c5f69ef8fa6a93de45fed2fa390bd82591f55
fariv indra <farivind@gmail.com>
2020-12-10 15:07:11 +0700

    setting connection

--- server/app.js
+++ server/app.js
@@ -1,20 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var pg = require("pg");
+var jwt = require("jsonwebtoken");
+var cors = require("cors");
 var app = express();
 
-app.use(logger('dev'));
+// ====== Setup local =======
+var config = require("./config");
+var user = require("./models/user");
+
+var indexRouter = require("./routes/index");
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+pg.connect(config.db);
+app.set("secretKey", config.secret);
+
+app.use("/", indexRouter);
 
 module.exports = app;
